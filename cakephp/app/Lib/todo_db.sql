57e55ae219bc128439e27ff1ec90b4d4822f6abe
yudaisaitou <yudai.saito@inter-plug.co.jp>
2015-07-10 02:32:26 +0900

    DB table

--- cakephp/app/Lib/todo_db.sql
+++ cakephp/app/Lib/todo_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 2015 年 7 朁E01 日 18:54
+-- Generation Time: 2015 年 7 朁E09 日 19:29
 -- サーバのバージョン： 5.6.24
 -- PHP Version: 5.6.8
 
@@ -49,8 +49,18 @@ INSERT INTO `account_tbs` (`id`, `name`, `pass`) VALUES
 
 CREATE TABLE IF NOT EXISTS `label_tbs` (
   `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(30) NOT NULL,
+  `account_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `label_tbs`
+--
+
+INSERT INTO `label_tbs` (`id`, `name`, `account_id`) VALUES
+(1, 'testlabel', 1),
+(2, 'labeltest', 1),
+(3, 'testlab', 1);
 
 -- --------------------------------------------------------
 
@@ -60,8 +70,18 @@ CREATE TABLE IF NOT EXISTS `label_tbs` (
 
 CREATE TABLE IF NOT EXISTS `project_tbs` (
   `id` int(11) NOT NULL,
-  `name` varchar(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `name` varchar(30) NOT NULL,
+  `account_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `project_tbs`
+--
+
+INSERT INTO `project_tbs` (`id`, `name`, `account_id`) VALUES
+(1, 'testpro', 1),
+(2, 'testproject', 1),
+(3, 'projecttest', 1);
 
 -- --------------------------------------------------------
 
@@ -72,11 +92,26 @@ CREATE TABLE IF NOT EXISTS `project_tbs` (
 CREATE TABLE IF NOT EXISTS `task_tbs` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL,
-  `memo` text NOT NULL,
-  `date` datetime NOT NULL,
-  `project_id` int(11) NOT NULL,
-  `label_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `memo` text,
+  `date` date DEFAULT NULL,
+  `completed` tinyint(1) NOT NULL,
+  `project_id` int(11) DEFAULT NULL,
+  `label_id` int(11) DEFAULT NULL,
+  `account_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+--
+-- テーブルのデータのダンプ `task_tbs`
+--
+
+INSERT INTO `task_tbs` (`id`, `name`, `memo`, `date`, `completed`, `project_id`, `label_id`, `account_id`) VALUES
+(1, 'testtask', 'ãƒ†ã‚¹ãƒˆã ã‚ˆãƒ¼', '2015-07-06', 0, 1, 1, 1),
+(2, 'ãƒ†ã‚¹ãƒˆã‚¿ã‚¹ã‚¯', 'ã¦ã™ã¨ã¦ã™ã¨ãƒ¼', '2015-07-07', 1, 1, 2, 1),
+(3, 'ã‚¿ã‚¹ã‚¯', 'ãŸã™ãã†ã†ã†ã†ã†ã†', '2015-07-08', 0, 3, 3, 1),
+(4, 'ã‚ã‚ã‚', 'ã‚ã‚ã‚', '2015-07-07', 0, 1, 2, 1),
+(5, 'ç ”ä¿®', 'ç ”ä¿®\r\n13ï¼š00', '2015-07-10', 0, 3, 3, 1),
+(6, 'booleantest', 'testboolean', '2015-07-08', 1, 2, 1, 1),
+(7, 'ã‚¿ã‚¹ã‚¯', 'aa', NULL, 0, 1, NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -119,17 +154,17 @@ ALTER TABLE `account_tbs`
 -- AUTO_INCREMENT for table `label_tbs`
 --
 ALTER TABLE `label_tbs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `project_tbs`
 --
 ALTER TABLE `project_tbs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `task_tbs`
 --
 ALTER TABLE `task_tbs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
